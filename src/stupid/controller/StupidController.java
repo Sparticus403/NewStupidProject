87a9dc59869a9ccc8a0bdaabb2238b3125e50aee
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-08 08:39:53 -0600

    added a private string to display my name.

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,17 @@
+package stupid.controller;
+
+public class StupidController 
+{
+	private String myName;
+	
+	public StupidController()
+	{
+		myName = "Todd Williamson";
+	}
+	
+	public void start()
+	{
+		System.out.println("the");
+		System.out.println(myName);
+	}
+}
